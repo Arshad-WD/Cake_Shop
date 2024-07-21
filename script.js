157840528e361ea67eb0276d274ba80baf84ae1a
ARSHAD CHAUDHARY <117998690+Arshad-WD@users.noreply.github.com>
2024-07-21 11:36:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loading = document.getElementById('loading');
+    const header = document.getElementById('header');
+    const hamburger = document.getElementById('hamburger');
+    const navbar = document.getElementById('navbar');
+
+    setTimeout(() => {
+        loading.style.display = 'none';
+        header.classList.remove('hidden');
+    }, 7000);
+
+    });
+    
